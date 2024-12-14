20a1a6e9b2d95cdc4d1ab73be357388cc1317d3f
Artem-0 <artiem.ruchii.99@mail.ru>
2024-12-14 23:47:15 +0300

    Отредактировал Задачу 4

--- ex4/Program.cs
+++ ex4/Program.cs
@@ -1,5 +1,29 @@
-﻿// Задание 4. Напишите программу, которая принимает на вход
-// три числа и выдаёт максимальное из этиъ чисел.
+﻿// Задача 4. Напишите программу, которая принимает на вход
+// три числа и выдаёт максимальное из этих чисел.
 
 System.Console.WriteLine("Введите первое число: ");
 int a = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+if (b > max)
+{
+    max = b;
+}
+else
+{
+    max = a;
+}
+
+if (c > max)
+{
+    max = c;
+    System.Console.WriteLine($"Максимальное число : {max}");
+}
+else
+{
+    System.Console.WriteLine($"Максимальное число : {max}");
+}
